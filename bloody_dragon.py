54c69b21e5ae9ff7b57204027f744273c6937fe4
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2021-07-20 05:10:16 +0000

    Update bloody_dragon.py

--- bloody_dragon.py
+++ bloody_dragon.py
@@ -5,7 +5,6 @@ g = "\033[32m"
 y = "\033[33m"
 b = "\033[34m"
 p = "\033[35m"
-
 banner=f"""{r} ▄▄▄▄    ██▓     ▒█████   ▒█████  ▓█████▄▓██   ██▓     
 ▓█████▄ ▓██▒    ▒██▒  ██▒▒██▒  ██▒▒██▀ ██▌▒██  ██▒     
 ▒██▒ ▄██▒██░    ▒██░  ██▒▒██░  ██▒░██   █▌ ▒██ ██░     
@@ -99,17 +98,40 @@ def lis():
     print(f"{r}-"*120+f"{g}")
     print("shell - for opening mobile shell")
     print(f"{r}-"*120+f"{g}")
+    print("keycode - for sending key")
+    print(f"{r}-"*120+f"{g}")
+    print("download - to download a file from phone")
+    print(f"{r}-"*120+f"{g}")
+    print("upload - to uplode a file from phone")
+    print(f"{r}-"*120+f"{g}")
     print("new tools adding soon!")
     print(f"{r}-"*120+f"{g}")
 def tools():
     print(banner)
     print(logo)
     while True:
-        print("enter \"tools\" for listing tools")
-        x=input("enter the tool:")
-        if x=="tools":
+        print(f"{g}[{r}0{g}]{y}list tools properties")
+        print(f"{g}[{r}1{g}]{y}screen")
+        print(f"{g}[{r}2{g}]{y}battery")
+        print(f"{g}[{r}3{g}]{y}activity")
+        print(f"{g}[{r}4{g}]{y}system info")
+        print(f"{g}[{r}5{g}]{y}type")
+        print(f"{g}[{r}6{g}]{y}net state")
+        print(f"{g}[{r}7{g}]{y}restart")
+        print(f"{g}[{r}8{g}]{y}wifi on")
+        print(f"{g}[{r}9{g}]{y}wifi off")
+        print(f"{g}[{r}10{g}]{y}openurl")
+        print(f"{g}[{r}11{g}]{y}adv prop")
+        print(f"{g}[{r}12{g}]{y}shell")
+        print(f"{g}[{r}13{g}]{y}download")
+        print(f"{g}[{r}14{g}]{y}upload")
+        print(f"{g}[{r}15{g}]{y}keycode")
+        print(f"{g}[{r}16{g}]{y}about Developers")
+        print(f"{g}[{r}99{g}]{y}exit")
+        x=input(f"enter the tool:{r}")
+        if x=="0":
             lis()
-        elif x=="screen":
+        elif x=="1":
             os.system("clear")
             print(banner)
             print(logo)
@@ -117,17 +139,17 @@ def tools():
             os.system("clear")
             print(banner)
             print(logo)   
-        elif x=="battery":
+        elif x=="2":
             os.system("clear")
             print(banner)
             print(logo)            
             os.system("adb shell dumpsys battery")
-        elif x=="activity":
+        elif x=="3":
             os.system("clear")
             print(banner)
             print(logo)            
             os.system("adb shell dumpsys activity")
-        elif x=="system info":
+        elif x=="4":
             os.system("clear")
             print(banner)
             print(logo)
@@ -139,35 +161,35 @@ def tools():
             os.system("adb shell getprop ro.build.version.release")
             print("Processor Architecture:")
             os.system("adb shell getprop getprop ro.product.cpu.abi")
-        elif x=="type":
+        elif x=="5":
             os.system("clear")
             print(banner)
             print(logo)
             k=input("enter the word to type:")
             os.system("adb shell input text "+k)
-        elif x=="net state":
+        elif x=="6":
             os.system("clear")
             print(banner)
             print(logo)
             os.system("adb shell netstat")
-        elif x=="restart":
+        elif x=="7":
             os.system("clear")
             print(banner)
             print(logo)
             os.system("adb shell reboot")
-        elif x=="wifi on":
+        elif x=="8":
             os.system("clear")
             print(banner)
             print(logo)
             print("done!")
             os.system("adb shell svc wifi enable")
-        elif x=="wifi off":
+        elif x=="9":
             os.system("clear")
             print(banner)
             print(logo)
             print("done!")
             os.system("adb shell svc wifi disable")
-        elif x=="openurl":
+        elif x=="10":
             os.system("clear")
             print(banner)
             print(logo)
@@ -176,26 +198,164 @@ def tools():
             os.system("clear")
             print(banner)
             print(logo)
-        elif x=="adv prop":
+        elif x=="11":
             os.system("clear")
             print(banner)
             print(logo)
             os.system("adb shell getprop | grep -e 'model' -e 'version.sdk' -e 'manufacturer' -e 'hardware' -e 'platform' -e 'revision' -e 'serialno' -e 'product.name' -e 'brand'")
-        elif x=="shell":
+        elif x=="12":
             os.system("clear")
             print(banner)
             print(logo)
             os.system("adb shell")
             os.system("clear")
             print(banner)
-            print(logo)          
+            print(logo)    
+        elif x=="13":
+            os.system("clear")
+            print(banner)
+            print(logo)
+            pu=input("Enter the file to download:")
+            os.system(f"adb pull {pu}")
+            os.system("clear")
+            print(banner)
+            print(logo)      
+        elif x=="14":
+            os.system("clear")
+            print(banner)
+            print(logo)
+            pu=input("Enter the file to upload:")
+            lo=input("Enter the folder in phone to upload:")
+            os.system(f"adb push {pu} {lo}")
+            os.system("clear")
+            print(banner)
+            print(logo)
+        elif x=="15":
+            codes=""""
+0 -->  "KEYCODE_0" 
+1 -->  "KEYCODE_SOFT_LEFT" 
+2 -->  "KEYCODE_SOFT_RIGHT" 
+3 -->  "KEYCODE_HOME" 
+4 -->  "KEYCODE_BACK" 
+5 -->  "KEYCODE_CALL" 
+6 -->  "KEYCODE_ENDCALL" 
+7 -->  "KEYCODE_0" 
+8 -->  "KEYCODE_1" 
+9 -->  "KEYCODE_2" 
+10 -->  "KEYCODE_3" 
+11 -->  "KEYCODE_4" 
+12 -->  "KEYCODE_5" 
+13 -->  "KEYCODE_6" 
+14 -->  "KEYCODE_7" 
+15 -->  "KEYCODE_8" 
+16 -->  "KEYCODE_9" 
+17 -->  "KEYCODE_STAR" 
+18 -->  "KEYCODE_POUND" 
+19 -->  "KEYCODE_DPAD_UP" 
+20 -->  "KEYCODE_DPAD_DOWN" 
+21 -->  "KEYCODE_DPAD_LEFT" 
+22 -->  "KEYCODE_DPAD_RIGHT" 
+23 -->  "KEYCODE_DPAD_CENTER" 
+24 -->  "KEYCODE_VOLUME_UP" 
+25 -->  "KEYCODE_VOLUME_DOWN" 
+26 -->  "KEYCODE_POWER" 
+27 -->  "KEYCODE_CAMERA" 
+28 -->  "KEYCODE_CLEAR" 
+29 -->  "KEYCODE_A" 
+30 -->  "KEYCODE_B" 
+31 -->  "KEYCODE_C" 
+32 -->  "KEYCODE_D" 
+33 -->  "KEYCODE_E" 
+34 -->  "KEYCODE_F" 
+35 -->  "KEYCODE_G" 
+36 -->  "KEYCODE_H" 
+37 -->  "KEYCODE_I" 
+38 -->  "KEYCODE_J" 
+39 -->  "KEYCODE_K" 
+40 -->  "KEYCODE_L" 
+41 -->  "KEYCODE_M" 
+42 -->  "KEYCODE_N" 
+43 -->  "KEYCODE_O" 
+44 -->  "KEYCODE_P" 
+45 -->  "KEYCODE_Q" 
+46 -->  "KEYCODE_R" 
+47 -->  "KEYCODE_S" 
+48 -->  "KEYCODE_T" 
+49 -->  "KEYCODE_U" 
+50 -->  "KEYCODE_V" 
+51 -->  "KEYCODE_W" 
+52 -->  "KEYCODE_X" 
+53 -->  "KEYCODE_Y" 
+54 -->  "KEYCODE_Z" 
+55 -->  "KEYCODE_COMMA" 
+56 -->  "KEYCODE_PERIOD" 
+57 -->  "KEYCODE_ALT_LEFT" 
+58 -->  "KEYCODE_ALT_RIGHT" 
+59 -->  "KEYCODE_SHIFT_LEFT" 
+60 -->  "KEYCODE_SHIFT_RIGHT" 
+61 -->  "KEYCODE_TAB" 
+62 -->  "KEYCODE_SPACE" 
+63 -->  "KEYCODE_SYM" 
+64 -->  "KEYCODE_EXPLORER" 
+65 -->  "KEYCODE_ENVELOPE" 
+66 -->  "KEYCODE_ENTER" 
+67 -->  "KEYCODE_DEL" 
+68 -->  "KEYCODE_GRAVE" 
+69 -->  "KEYCODE_MINUS" 
+70 -->  "KEYCODE_EQUALS" 
+71 -->  "KEYCODE_LEFT_BRACKET" 
+72 -->  "KEYCODE_RIGHT_BRACKET" 
+73 -->  "KEYCODE_BACKSLASH" 
+74 -->  "KEYCODE_SEMICOLON" 
+75 -->  "KEYCODE_APOSTROPHE" 
+76 -->  "KEYCODE_SLASH" 
+77 -->  "KEYCODE_AT" 
+78 -->  "KEYCODE_NUM" 
+79 -->  "KEYCODE_HEADSETHOOK" 
+80 -->  "KEYCODE_FOCUS" 
+81 -->  "KEYCODE_PLUS" 
+82 -->  "KEYCODE_MENU" 
+83 -->  "KEYCODE_NOTIFICATION" 
+84 -->  "KEYCODE_SEARCH" 
+85 -->  "KEYCODE_MEDIA_PLAY_PAUSE"
+86 -->  "KEYCODE_MEDIA_STOP"
+87 -->  "KEYCODE_MEDIA_NEXT"
+88 -->  "KEYCODE_MEDIA_PREVIOUS"
+89 -->  "KEYCODE_MEDIA_REWIND"
+90 -->  "KEYCODE_MEDIA_FAST_FORWARD"
+91 -->  "KEYCODE_MUTE"
+92 -->  "KEYCODE_PAGE_UP"
+93 -->  "KEYCODE_PAGE_DOWN"
+94 -->  "KEYCODE_PICTSYMBOLS"
+122 -->  "KEYCODE_MOVE_HOME"
+123 -->  "KEYCODE_MOVE_END"
+"""
+            os.system("clear")
+            print(banner)
+            print(logo)
+            print(codes)
+            pu=input("Enter keycode:")
+            os.system(f"adb shell input keyevent {pu}")
+            os.system("clear")
+            print(banner)
+            print(logo)
+        elif x=="16":
+            os.system("clear")
+            print(banner)
+            print(logo)
+            print("script by BLINKING-IDIOT")
+            print("Insta : @a.r.n_beatzzzz")
+            print("thanks for using this tool!")
+            print("please give a star to this tool!")
+        elif x=="99":
+            exit()
         else:
             os.system("clear")
             print(banner)
             print(logo)
             print("not a tool...retry")
 def main():
-    x=input("is your mobile connected with ADB?[y/n]")
+    x=input("Is your mobile connected with ADB?[y/n]")
     if x=="n":
         connect()
     os.system("clear")
